e1012cb1d59f3407f09cd414d6ff2a204c60cf3f
Niklas Olsson <hoyce@kth.se>
2021-04-15 09:05:31 +0200

    Prettier and ESLint fixes

--- index.js
+++ index.js
@@ -1,26 +1,39 @@
-var messages = []
-var _DEFAULT_LANG = 'sv'
+const messages = []
+const _DEFAULT_LANG = 'sv'
 
 /**
  * Fetches a language by its short name.
  * Returns undefined if no language was found.
  */
-function _getLanguageByShortname (shortName) {
-  for (var i in messages) {
-    for (var u in messages[ i ].shortNames) {
-      if (shortName && messages[ i ].shortNames[ u ].toLowerCase() === shortName.toLowerCase()) {
-        return messages[ i ]
+function _getLanguageByShortname(shortName) {
+  for (const i in messages) {
+    for (const u in messages[i].shortNames) {
+      if (shortName && messages[i].shortNames[u].toLowerCase() === shortName.toLowerCase()) {
+        return messages[i]
       }
     }
   }
   return undefined
 }
 
+function getCookie(cname) {
+  if (typeof document === 'undefined') {
+    return
+  }
+  const name = cname + '='
+  const ca = document.cookie.split(';')
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i]
+    while (c.charAt(0) === ' ') c = c.substring(1)
+    if (c.indexOf(name) === 0) return c.substring(name.length, c.length)
+  }
+}
+
 /**
  * Returns the message for a given key for the active language
  */
-function _message (key, overrideLang) {
-  var language = overrideLang || getCookie('language') || _DEFAULT_LANG
+function _message(key, overrideLang) {
+  const language = overrideLang || getCookie('language') || _DEFAULT_LANG
   // If no key is provided we'll just assume that
   // we want an empty string back
   if (!key) {
@@ -28,52 +41,37 @@ function _message (key, overrideLang) {
   }
 
   // Try to find a language. Use the default (se) if missing.
-  var lang = _getLanguageByShortname(language) ||
-  _getLanguageByShortname(_DEFAULT_LANG)
+  const lang = _getLanguageByShortname(language) || _getLanguageByShortname(_DEFAULT_LANG)
 
   // Make sure we see if a key is missing
-  if (lang.messages[ key ] === undefined) {
-    return 'KEY ' + key + ' FOR LANGUAGE ' +
-    lang.longNameEn + ' DOES NOT EXIST'
+  if (lang.messages[key] === undefined) {
+    return 'KEY ' + key + ' FOR LANGUAGE ' + lang.longNameEn + ' DOES NOT EXIST'
   }
 
-  return lang.messages[ key ]
-}
-
-function getCookie (cname) {
-  if (typeof document === 'undefined') {
-    return
-  }
-  var name = cname + '='
-  var ca = document.cookie.split(';')
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[ i ]
-    while (c.charAt(0) === ' ') c = c.substring(1)
-    if (c.indexOf(name) === 0) return c.substring(name.length, c.length)
-  }
+  return lang.messages[key]
 }
 
 /**
  * Return true if the current language is Swedish
  */
-function _isSwedish () {
-  var language = getCookie('language')
+function _isSwedish() {
+  const language = getCookie('language')
   return language === 'sv' || language === ''
 }
 
 /**
  * Return true if the current language is English
  */
-function _isEnglish () {
-  var language = getCookie('language')
+function _isEnglish() {
+  const language = getCookie('language')
   return language === 'en'
 }
 
 module.exports = {
   getLanguageByShortname: _getLanguageByShortname,
   DEFAULT_LANG: _DEFAULT_LANG,
-  messages: messages,
+  messages,
   message: _message,
   isSwedish: _isSwedish,
-  isEnglish: _isEnglish
+  isEnglish: _isEnglish,
 }
--- test/test.js
+++ test/test.js
@@ -1,50 +1,51 @@
 /* eslint-env mocha */
 /* eslint-disable no-unused-expressions */
-const expect = require('chai').expect
+const { expect } = require('chai')
 const i18n = require('../index')
+
 i18n.messages.push(
   {
     shortNames: ['en', 'us'],
     longNameSe: 'Engelska',
     longNameEn: 'English',
     messages: {
-      message: 'Message'
-    }
+      message: 'Message',
+    },
   },
   {
     shortNames: ['sv', 'se'],
     longNameSe: 'Svenska',
     longNameEn: 'Swedish',
     messages: {
-      message: 'Meddelande'
-    }
+      message: 'Meddelande',
+    },
   }
 )
-describe('Testing i18n', function () {
-  it('should get a message by language', function () {
+describe('Testing i18n', () => {
+  it('should get a message by language', () => {
     const msg = i18n.message('message', 'en')
     expect(msg).to.be.equal('Message')
   })
 
-  it('should get a message with default language', function () {
+  it('should get a message with default language', () => {
     const msg = i18n.message('message')
     expect(msg).to.be.equal('Meddelande')
   })
 
-  it('should return a message if key does not exist', function () {
+  it('should return a message if key does not exist', () => {
     const msg = i18n.message('blabla')
     expect(msg).to.be.equal('KEY blabla FOR LANGUAGE Swedish DOES NOT EXIST')
   })
 
-  it('should get a message with default language on browser', function () {
+  it('should get a message with default language on browser', () => {
     global.document = {
-      cookie: ' language=en;blabla=blabla'
+      cookie: ' language=en;blabla=blabla',
     }
     const msg = i18n.message('message')
     expect(msg).to.be.equal('Message')
   })
-  it('should provide helper methods for determining language', function () {
-    var lang = i18n.isSwedish()
+  it('should provide helper methods for determining language', () => {
+    let lang = i18n.isSwedish()
     expect(lang).to.be.false
     lang = i18n.isEnglish()
     expect(lang).to.be.true
